78cf876f277a91d855a1c80b5dfd7d3c63d6d0ba
Shakib Absar <20sa32@queensu.ca>
2023-02-02 17:19:59 -0500

    all func in cagey_csp returns csp model with the Variables, still need to implement the constraints

--- A1/cagey_csp.py
+++ A1/cagey_csp.py
@@ -84,16 +84,95 @@ An example of a 3x3 puzzle would be defined as:
 '''
 
 from cspbase import *
+import os
 
 def binary_ne_grid(cagey_grid):
     ##IMPLEMENT
-    pass
-
+    return 0
 
 def nary_ad_grid(cagey_grid):
-    ## IMPLEMENT
-    pass
+    
+    n, cages = cagey_grid
+    
+    name = "nary_ad_{}".format(n)
+    cells = create_cell_variables(n)
+    cage_vars = create_cage_variables(cages)
+    
+    vars = []
+    for row in cells:
+        vars += row
+        
+    vars += cage_vars
+    
+    # for v in vars:
+    #     print(type(v))
+    
+    csp_model = CSP(name, vars)
+    return csp_model
 
 def cagey_csp_model(cagey_grid):
     ##IMPLEMENT
     pass
+    
+
+# Return nXn matrix of cell Variables with the proper domain and name
+def create_cell_variables(n):
+    cells = []
+    cell_domain = list(range(1, n+1))
+    for i in range(n):
+        row = []
+        for j in range(n):
+            cell = Variable("Cell({},{})".format(i+1,j+1), cell_domain)
+            row.append(cell)
+        cells.append(row)
+        
+    return cells
+
+# Return all the cage varibles for the cages
+def create_cage_variables(cages):
+    cage_domain = ["+", "-", "*", "/"]
+    cage_vars = []
+    
+    for cage in cages:
+        v, cells, op = cage
+        cell_text = ""
+        
+        for cell in cells:
+            cell_text += "Var-Cell({},{}), ".format(cell[0], cell[1])
+        cell_text = "[{}]".format(cell_text.rstrip(", "))
+        
+        
+        var_name = "Cage_op({}:{}:{})".format(v, op, cell_text)
+        
+        cage_vars.append(Variable(var_name, cage_domain))
+        
+    return cage_vars
+    
+    
+
+if __name__ == "__main__":
+    # Sample grid to test our code
+    sample_grid = (3, [(3,[(1,1), (2,1)],"+"),(1, [(1,2)], '?'), (8, [(1,3), (2,3), (2,2)], "+"), (3, [(3,1)], '?'), (3, [(3,2), (3,3)], "+")])
+    # print(sample_grid)
+    
+
+    # model = binary_ne_grid(sample_grid)
+    model = nary_ad_grid(sample_grid)
+    # model = cagey_csp_model(sample_grid)
+    
+    
+    for var in model.get_all_vars():
+        print(var)
+        
+    # for con in model.get_all_cons():
+    #     print(con)
+
+    # temp = create_cell_variables(3)
+
+    # for i in temp:
+    #     for j in i:
+    #         print(j.domain())
+    
+    # b = create_cage_variables(sample_grid[1])
+    # for t in b:
+    #     print(t.domain())
